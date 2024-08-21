26b0ff71e8bece3370a9338584526db43554108b
Matt King <60826820+kingmatte@users.noreply.github.com>
2024-08-21 12:54:07 -0400

    finishing test

--- tests/fetch/fetchTest.test.ts
+++ tests/fetch/fetchTest.test.ts
@@ -24,6 +24,7 @@ import { expect, test } from '@playwright/test'
 let bars = ['0','1','2','3','4','5','6','7']
 let leftBars = bars.slice(0, 4)
 let rightBars = bars.slice(-4)
+const weighings: string[] = []
 let fakeBar = ''
 
 test.describe('fetch sdet challenge', () => {
@@ -46,6 +47,7 @@ test.describe('fetch sdet challenge', () => {
     await test.step('check result of initial weighing', async () => {
       await weighButton.click();
       await result.waitFor({state: 'visible', timeout: 5_000})
+      weighings.push(`${leftBars} ${await result.textContent()} ${rightBars}`)
       if (await result.textContent() === '=') {
         fakeBar = '8'
       } else {
@@ -57,30 +59,35 @@ test.describe('fetch sdet challenge', () => {
       }
     })
     if (fakeBar === '') {
-      await test.step('maybe fill in next batch of bars and weigh', async () => {
-        // foreach instead
+      await test.step('fill in next batch of bars and weigh', async () => {
         for (let index = 0; index < 2; index+=1) {
             await page.locator(`input#left_${index}`).fill(leftBars[index])
             await page.locator(`input#right_${index}`).fill(rightBars[index])
         }
         await weighButton.click();
         await result.waitFor({state: 'visible', timeout: 5_000})
-        bars = await result.textContent() === '>' ? rightBars : leftBars
+        weighings.push(`${leftBars} ${await result.textContent()} ${rightBars}`)
+        bars = await result.textContent() === '<' ? leftBars : rightBars
         leftBars = [bars[0]]
         rightBars = [bars[1]]
         await resetButton.click()
         await page.locator('button:has-text("?")').waitFor({state: 'visible', timeout: 5_000})
       })
-      await test.step('maybe fill in third batch of bars and weigh', async () => {
+      await test.step('fill in third batch of bars and weigh', async () => {
           await page.locator(`input#left_0`).fill(leftBars[0])
           await page.locator(`input#right_0`).fill(rightBars[0])
           await weighButton.click();
           await page.locator('button:has-text(">"), button:has-text("<")').waitFor({state: 'visible', timeout: 5_000})
-          fakeBar = await result.textContent() === '>' ? leftBars[0] : rightBars[0]
+          weighings.push(`${leftBars} ${await result.textContent()} ${rightBars}`)
+          fakeBar = await result.textContent() === '<' ? leftBars[0] : rightBars[0]
       })
     }
     await test.step('verify results and log', async () => {
-      console.log(fakeBar)
+      console.log(`The fake bar number is: ${fakeBar}`)
+      console.log('Weighings:')
+      weighings.forEach((weighing) => {
+        console.log(weighing)
+      })
       page.on('dialog', async (dialog) => {
         expect(dialog.message()).toContain('Yay!');
         await dialog.accept();
