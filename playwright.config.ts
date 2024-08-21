eb0b490ae9f3f08d08d1d5de499fbc33f0faf7a4
Matt King <60826820+kingmatte@users.noreply.github.com>
2024-08-21 11:17:21 -0400

    test updates

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+// playwright.config.ts
+import { chromium, PlaywrightTestConfig } from '@playwright/test';
+
+type BrowserName = 'chromium' | 'firefox' | 'webkit';
+
+const config: PlaywrightTestConfig = {
+	outputDir: 'tests/test-results',
+	globalTimeout: 120 * 60 * 1000, // two hours per run to start
+	expect: { timeout: 5 * 1000 }, // 5 seconds default for expect statement to resolve
+	timeout: 1.5 * 60 * 1000, // 90 second as a base limit per test. Can be changed in test files with test.setTimeout()
+	retries: 0,
+	maxFailures: 10,
+	testMatch: /.*\.test\.ts/,
+	workers: 10,
+	reporter: [
+		['json', { outputFile: `tests/playwright-results/results.json`, open: 'never' }],
+		['html', { open: 'never', outputFolder: 'tests/playwright-report' }],
+	],
+	use: {
+		trace: 'retain-on-failure',
+		headless: true,
+		browserName: 'chromium',
+		viewport: { width: 1440, height: 1080 },
+		screenshot: 'only-on-failure',
+		video: 'retain-on-failure',
+		storageState: 'tests/test-storage/default.json',
+		actionTimeout: 30 * 1000, // 30 seconds default for actions like clicks to resolve
+		navigationTimeout: 30 * 1000, // 30 seconds default for navigation to complete
+		testIdAttribute: 'id',
+	},
+};
+// eslint-disable-next-line import/no-default-export
+export default config;
--- tests/fetch/fetchTest.test.ts
+++ tests/fetch/fetchTest.test.ts
@@ -19,24 +19,58 @@
 // e. Clicking on the gold bar number at the bottom of the website and checking for the alert message
 // 3. Code the algorithm from step 1 which uses a set of actions from step 2 to find the fake gold bar
 // The algorithm should populate and weigh gold bars until a fake on
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 const bars = ['0','1','2','3','4','5','6','7','8']
+let filteredBars: string[] = []
 let fakeBar = ''
 
 test.describe('fetch sdet challenge', () => {
   test.beforeEach('test setup', async ({ page }) => {
-    await page.goto('https://sdetchallenge.fetch.com', {waitUntil: 'load'})
-    for (let index = 0; index < 8; index+=1) {
-      if (index < 4) {
-        await page.locator(`input#left_${index}`).fill(bars[index])
-      } else {
-        await page.locator(`input#right_${index}`).fill(bars[index])
+    await test.step('navigate to page', async () => {
+      await page.goto('https://sdetchallenge.fetch.com', {waitUntil: 'load'})
+    })
+    await test.step('fill in initial bars to be weighed', async () => {
+      for (let index = 0; index < 8; index+=1) {
+        if (index < 4) {
+          await page.locator(`input#left_${index}`).fill(bars[index])
+        } else {
+          await page.locator(`input#right_${index-4}`).fill(bars[index])
+        }
       }
-    }
+    })
   })
 
-  test('should select fake gold bar', async () => {
-    //
+  test('should select fake gold bar', async ({ page }) => {
+    const weighButton = page.getByRole('button', {name: 'Weigh'})
+    const resetButton = page.getByRole('button', {name: 'Reset'})
+    const equalResult = page.getByRole('button', {name: '='})
+    filteredBars = ['0','1','2','3']
+    await test.step('check result of initial weighing', async () => {
+      await weighButton.click();
+      await equalResult.waitFor({state: 'visible', timeout: 10_000})
+      await resetButton.click();
+    })
+    await test.step('fill in next batch of bars', async () => {
+      for (let index = 0; index < 4; index+=1) {
+        if (index < 2) {
+          await page.locator(`input#left_${index}`).fill(bars[index])
+        } else {
+          await page.locator(`input#right_${index-2}`).fill(bars[index])
+        }
+      }
+    })
+    await test.step('weigh bars', async () => {
+      await weighButton.click();
+      await equalResult.waitFor({state: 'visible', timeout: 10_000});
+    })
+    await test.step('verify results and log', async () => {
+      fakeBar = '0'
+      page.on('dialog', async (dialog) => {
+        expect(dialog.message()).toContain('Yay!');
+        await dialog.accept();
+      });
+      await page.getByRole('button', {name: fakeBar}).click();
+    })
   })
 })
