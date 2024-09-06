661dce23d8f539d81f09cf9fed8ebec55706d44f
Matt King <60826820+kingmatte@users.noreply.github.com>
2024-09-06 12:55:27 -0400

    Update checkDMV.test.ts

--- tests/playwright/dmv/checkDMV.test.ts
+++ tests/playwright/dmv/checkDMV.test.ts
@@ -1,7 +1,21 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
-test.describe('', () => {
-    test('', async ({ page }) => {
-        await page.goto('https://www.ncdot.gov/dmv/license-id/driver-license-appointments/Pages/default.aspx')
+test.describe('Check DMV status', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://skiptheline.ncdot.gov/')
+    })
+    test('Visit DMV and check for appointments', async ({ page }) => {
+        await test.step('Open DMV scheduling page', async () => {
+            await page.getByRole('button', {name: 'Make an Appointment'}).click();
+            await page.getByText('Driver License Renewal').click()
+        })
+        await test.step('Enter search criteria', async () => {
+            await page.getByRole('textbox', { name: 'Search' }).pressSequentially('27517')
+            await page.getByText('27517 Chapel Hill').click()
+        })
+        await test.step('', async () => {
+            //
+        })
+        expect(page.url()).toContain('wut')
     })
 })
